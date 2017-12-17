4fc34211b9cac79bb54bff9fd4105d066b147c76
Milad Irannejad <moorara@gmail.com>
2017-12-16 23:41:22 -0500

    Use reflection for Trust function

--- pki/trust.go
+++ pki/trust.go
@@ -7,23 +7,65 @@ import (
 )
 
 var (
-	regexCommonName         = regexp.MustCompile("(?i)^Common[_-]?Name$")
-	regexCountry            = regexp.MustCompile("(?i)^Country$")
-	regexProvince           = regexp.MustCompile("(?i)^Province$")
-	regexLocality           = regexp.MustCompile("(?i)^Locality$")
-	regexOrganization       = regexp.MustCompile("(?i)^Organization$")
-	regexOrganizationalUnit = regexp.MustCompile("(?i)^Organizational[_-]?Unit$")
-	regexDNSName            = regexp.MustCompile("(?i)^DNS[_-]?Name(s)?$")
-	regexIPAddress          = regexp.MustCompile("(?i)^IP[_-]?Address(es)?$")
-	regexEmailAddress       = regexp.MustCompile("(?i)^Email[_-]?Address(es)?$")
-	regexStreetAddress      = regexp.MustCompile("(?i)^Street[_-]?Address$")
-	regexPostalCode         = regexp.MustCompile("(?i)^Postal[_-]?Code$")
+	regex = map[string]*regexp.Regexp{
+		"CommonName":         regexp.MustCompile("(?i)^Common[_-]?Name$"),
+		"Country":            regexp.MustCompile("(?i)^Country$"),
+		"Province":           regexp.MustCompile("(?i)^Province$"),
+		"Locality":           regexp.MustCompile("(?i)^Locality$"),
+		"Organization":       regexp.MustCompile("(?i)^Organization$"),
+		"OrganizationalUnit": regexp.MustCompile("(?i)^Organizational[_-]?Unit$"),
+		"DNSNames":           regexp.MustCompile("(?i)^DNS[_-]?Name(s)?$"),
+		"IPAddresses":        regexp.MustCompile("(?i)^IP[_-]?Address(es)?$"),
+		"EmailAddresses":     regexp.MustCompile("(?i)^Email[_-]?Address(es)?$"),
+		"StreetAddress":      regexp.MustCompile("(?i)^Street[_-]?Address$"),
+		"PostalCode":         regexp.MustCompile("(?i)^Postal[_-]?Code$"),
+	}
 )
 
 type (
 	// TrustFunc is the function for determing if a ca can sign a csr
 	TrustFunc func(*x509.Certificate, *x509.CertificateRequest) bool
 )
+
+func matches(cert *x509.Certificate, req *x509.CertificateRequest, fieldName string) bool {
+	zero := reflect.Value{}
+
+	certV := reflect.ValueOf(cert).Elem()
+	certFV := certV.FieldByName(fieldName)
+	if certFV == zero {
+		certFV = certV.FieldByName("Subject").FieldByName(fieldName)
+	}
+
+	reqV := reflect.ValueOf(req).Elem()
+	reqFV := reqV.FieldByName(fieldName)
+	if reqFV == zero {
+		reqFV = reqV.FieldByName("Subject").FieldByName(fieldName)
+	}
+
+	if certFV == zero || reqFV == zero || !reflect.DeepEqual(certFV.Interface(), reqFV.Interface()) {
+		return false
+	}
+
+	return true
+}
+
+func supplied(req *x509.CertificateRequest, fieldName string) bool {
+	zero := reflect.Value{}
+
+	reqV := reflect.ValueOf(req).Elem()
+	reqFV := reqV.FieldByName(fieldName)
+	if reqFV == zero {
+		reqFV = reqV.FieldByName("Subject").FieldByName(fieldName)
+	}
+
+	if (reqFV == zero) ||
+		(reqFV.Kind() == reflect.String && reqFV.String() == "") ||
+		(reqFV.Kind() == reflect.Slice && reqFV.Len() == 0) {
+		return false
+	}
+
+	return true
+}
 
 // PolicyTrustFunc returns a TrustFunc using Policy
 func PolicyTrustFunc(policy Policy) TrustFunc {
@@ -33,120 +75,18 @@ func PolicyTrustFunc(policy Policy) TrustFunc {
 		}
 
 		// These fields should be matched
-		for _, name := range policy.Match {
-			switch {
-			case regexCommonName.MatchString(name):
-				if ca.Subject.CommonName != csr.Subject.CommonName {
-					return false
-				}
-
-			case regexCountry.MatchString(name):
-				if !reflect.DeepEqual(ca.Subject.Country, csr.Subject.Country) {
-					return false
-				}
-
-			case regexProvince.MatchString(name):
-				if !reflect.DeepEqual(ca.Subject.Province, csr.Subject.Province) {
-					return false
-				}
-
-			case regexLocality.MatchString(name):
-				if !reflect.DeepEqual(ca.Subject.Locality, csr.Subject.Locality) {
-					return false
-				}
-
-			case regexOrganization.MatchString(name):
-				if !reflect.DeepEqual(ca.Subject.Organization, csr.Subject.Organization) {
-					return false
-				}
-
-			case regexOrganizationalUnit.MatchString(name):
-				if !reflect.DeepEqual(ca.Subject.OrganizationalUnit, csr.Subject.OrganizationalUnit) {
-					return false
-				}
-
-			case regexDNSName.MatchString(name):
-				if !reflect.DeepEqual(ca.DNSNames, csr.DNSNames) {
-					return false
-				}
-
-			case regexIPAddress.MatchString(name):
-				if !reflect.DeepEqual(ca.IPAddresses, csr.IPAddresses) {
-					return false
-				}
-
-			case regexEmailAddress.MatchString(name):
-				if !reflect.DeepEqual(ca.EmailAddresses, csr.EmailAddresses) {
-					return false
-				}
-
-			case regexStreetAddress.MatchString(name):
-				if !reflect.DeepEqual(ca.Subject.StreetAddress, csr.Subject.StreetAddress) {
-					return false
-				}
-
-			case regexPostalCode.MatchString(name):
-				if !reflect.DeepEqual(ca.Subject.PostalCode, csr.Subject.PostalCode) {
+		for _, field := range policy.Match {
+			for key, re := range regex {
+				if re.MatchString(field) && !matches(ca, csr, key) {
 					return false
 				}
 			}
 		}
 
 		// These fields should be present
-		for _, name := range policy.Supplied {
-			switch {
-			case regexCommonName.MatchString(name):
-				if csr.Subject.CommonName == "" {
-					return false
-				}
-
-			case regexCountry.MatchString(name):
-				if csr.Subject.Country == nil || len(csr.Subject.Country) == 0 {
-					return false
-				}
-
-			case regexProvince.MatchString(name):
-				if csr.Subject.Province == nil || len(csr.Subject.Province) == 0 {
-					return false
-				}
-
-			case regexLocality.MatchString(name):
-				if csr.Subject.Locality == nil || len(csr.Subject.Locality) == 0 {
-					return false
-				}
-
-			case regexOrganization.MatchString(name):
-				if csr.Subject.Organization == nil || len(csr.Subject.Organization) == 0 {
-					return false
-				}
-
-			case regexOrganizationalUnit.MatchString(name):
-				if csr.Subject.OrganizationalUnit == nil || len(csr.Subject.OrganizationalUnit) == 0 {
-					return false
-				}
-
-			case regexDNSName.MatchString(name):
-				if csr.DNSNames == nil || len(csr.DNSNames) == 0 {
-					return false
-				}
-
-			case regexIPAddress.MatchString(name):
-				if csr.IPAddresses == nil || len(csr.IPAddresses) == 0 {
-					return false
-				}
-
-			case regexEmailAddress.MatchString(name):
-				if csr.EmailAddresses == nil || len(csr.EmailAddresses) == 0 {
-					return false
-				}
-
-			case regexStreetAddress.MatchString(name):
-				if csr.Subject.StreetAddress == nil || len(csr.Subject.StreetAddress) == 0 {
-					return false
-				}
-
-			case regexPostalCode.MatchString(name):
-				if csr.Subject.PostalCode == nil || len(csr.Subject.PostalCode) == 0 {
+		for _, field := range policy.Supplied {
+			for key, re := range regex {
+				if re.MatchString(field) && !supplied(csr, key) {
 					return false
 				}
 			}
